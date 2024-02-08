c748126c4b04f3c6e461b740b02a4837d59f375d
Sazzad192 <sazadulbg130@gmail.com>
2024-02-08 21:24:02 +0600

    add homepage

---
+++ app/components/NavLinks.jsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import RightSideDrawer from "@/components/RightSideDrawer";
+import { Bars4Icon } from "@heroicons/react/24/outline";
+import { usePathname } from "next/navigation";
+
+const navLinks = [
+  { label: "Home", url: "/" },
+  { label: "Task List", url: "/tasks" },
+  { label: "Create Task", url: "/tasks/create" },
+];
+
+export default function NavLinks() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const activeRoute = usePathname();
+  const isActiveRoute = (activeNavLink) => activeRoute === activeNavLink;
+
+  return (
+    <div className="flex items-center gap-4">
+      <Bars4Icon
+        onClick={() => setDrawerOpen(true)}
+        className="block h-7 w-7 cursor-pointer md:hidden"
+        aria-hidden="true"
+      />
+      {navLinks.map((item) => (
+        <Link
+          href={item.url}
+          className={`hidden md:flex text-gray-600 hover:bg-gray-50 hover:text-gray-900 group items-center px-3 py-2 text-sm font-medium rounded-md ${
+            isActiveRoute(item.url) &&
+            "bg-primary-100 hover:bg-primary-100 text-primary hover:text-primary"
+          }`}
+        >
+          {item.label}
+        </Link>
+      ))}
+
+      <RightSideDrawer title="ToDo" open={drawerOpen} setOpen={setDrawerOpen}>
+        <div className="">
+          {navLinks.map((item) => (
+            <Link
+              href={item.url}
+              className={`text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-3 py-2 text-sm font-medium rounded-md ${
+                isActiveRoute(item.url) &&
+                "bg-primary-100 hover:bg-primary-100 text-primary hover:text-primary"
+              }`}
+            >
+              {item.label}
+            </Link>
+          ))}
+        </div>
+      </RightSideDrawer>
+    </div>
+  );
+}
--- components/BaseNavbar.jsx
+++ components/BaseNavbar.jsx
@@ -1,3 +1,4 @@
+import NavLinks from "@/app/components/NavLinks";
 import Link from "next/link";
 
 function BaseNavbar() {
@@ -5,12 +6,12 @@ function BaseNavbar() {
     <nav className="sticky top-0 p-4 bg-white border-b border-gray-200 lg:border-none">
       <div className="flex justify-between items-center mx-auto max-w-7xl">
         <div className="flex items-center gap-10">
-          <Link className="font-semibold text-lg" href="/">ToDo</Link>
-        </div>
-        <div className="flex items-center gap-4">
-          <Link href="/" className="text-primary-800 font-bold text-base">
-            Home
+          <Link className="font-semibold text-lg" href="/">
+            ToDo
           </Link>
+        </div>
+        <div id="nav link">
+          <NavLinks/>
         </div>
       </div>
     </nav>
