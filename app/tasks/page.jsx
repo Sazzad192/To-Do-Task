2c2a042eb2a660828e1916b8e9503b4d4e2ffc13
sazzad192 <sazadulbg130@gmail.com>
2024-02-09 12:44:48 +0600

    fixed build issue

--- app/tasks/page.jsx
+++ app/tasks/page.jsx
@@ -5,10 +5,18 @@ import PageTitle from "@/components/PageTitle";
 import { PlusIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/navigation";
 import TaskCard from "./components/TaskCard";
+import { useEffect, useState } from "react";
 
 export default function TaskPage() {
   const router = useRouter();
-  const taskList = getTaskList();
+const [taskList,setTaskList]=useState(null)
+
+useEffect(()=>{
+
+  setTaskList(getTaskList())
+
+},[])
+
 
   return (
     <div className="mx-auto p-4 w-full sm:px-6 lg:px-10 max-w-7xl min-h-screen">
@@ -21,7 +29,7 @@ export default function TaskPage() {
       </section>
 
       <section className="space-y-3 pt-3">
-        {taskList.length ? (
+        {taskList?.length ? (
           taskList.map((item, i) => <TaskCard key={i} data={item} />)
         ) : (
           <p className="text-center font-semibold"> No Task Added Yet</p>
